c404b4c80028dc305a38572af338ee7a19ea6566
SidharthSKR99 <sidhukrajesh@gmail.com>
2025-01-21 22:27:36 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -5,11 +5,12 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
     
     
-    const validUsername = 'user123';
-    const validPassword = 'password123';
+    const validUsername = 'user';
+    const validPassword = 'password';
     
     if (username === validUsername && password === validPassword) {
         alert('Login successful');
+        document.getElementById('login-image').src = 'https://preview.redd.it/8vhs36c6d3o51.jpg?auto=webp&s=d3eecf10fb47e0e460dc0e8da3143017f650effb';
     } else {
         document.getElementById('error-message').textContent = 'Invalid username or password(Fraudulent Behaviour)';
     }
